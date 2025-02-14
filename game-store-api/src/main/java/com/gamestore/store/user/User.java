6857deefb936a4f6a10d1f29c3cd3ca8ac3752b5
PatelPratik39 <ptl.pratik717@gmail.com>
2025-02-14 01:59:09 -0600

    App is up and running

--- game-store-api/src/main/java/com/gamestore/store/user/User.java
+++ game-store-api/src/main/java/com/gamestore/store/user/User.java
@@ -2,6 +2,7 @@ package com.gamestore.store.user;
 
 import com.gamestore.store.common.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "users")
 public class User extends BaseEntity {
 
     private String firstName;
